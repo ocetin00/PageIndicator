ec6c6c41b3de86a0f07fdf896aec3aa91595e4a2
ocetin <ocetin169@gmail.com>
2023-02-11 17:14:17 +0300

    code refactor

--- page_indicator/src/main/java/com/oguzhancetin/page_indicator/PageIndicator.kt
+++ page_indicator/src/main/java/com/oguzhancetin/page_indicator/PageIndicator.kt
@@ -12,7 +12,8 @@ import androidx.compose.material.icons.filled.Check
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.Saver
-import androidx.compose.runtime.saveable.listSaver
+import androidx.compose.runtime.saveable.mapSaver
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -20,20 +21,61 @@ import androidx.compose.ui.graphics.*
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import kotlinx.coroutines.launch
 
 /**
  * Created by ocetin00 on 6.02.2023
  */
+@OptIn(ExperimentalMaterial3Api::class)
 @Preview
 @Composable
 fun Home() {
-    val state = PageIndicatorState(listOf("First", "Second", "Third", "Fourth"))
-    Screen(state)
+    val state = rememberPageIndicatorState(listOf("First", "Second", "Third", "Fourth"))
+    HomeContent(state)
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview
+@Composable
+fun PageIndicatorPreview(
+    pageIndicatorState: PageIndicatorState = rememberPageIndicatorState(
+        listOf(
+            "First",
+            "Second",
+            "Third",
+            "Fourth"
+        )
+    )
+
+) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+    ) {
+        Row() {
+            PageIndicator(
+                pageIndicatorState = pageIndicatorState,
+                indicatorColor = MaterialTheme.colorScheme.primary,
+                onIndicatorColor = MaterialTheme.colorScheme.onPrimary,
+            )
+        }
+        Row() {
+            Button(onClick = { pageIndicatorState.onPrevious() }) {
+                Text("Previous")
+            }
+            Button(onClick = { pageIndicatorState.onNext() }) {
+                Text("Next")
+            }
+
+        }
+
+        Text("current page: ${pageIndicatorState.current + 1}")
+    }
+
 }
 
 @Composable
-fun Screen(state: PageIndicatorState) {
+fun HomeContent(
+    state: PageIndicatorState
+) {
     Column(modifier = Modifier.fillMaxSize()) {
         PageIndicator(state)
         Box(contentAlignment = Alignment.BottomCenter) {
@@ -44,7 +86,7 @@ fun Screen(state: PageIndicatorState) {
                     modifier = Modifier
                         .align(Alignment.BottomStart)
                         .padding(15.dp),
-                    onClick = { state.onPreviousClick() }) {
+                    onClick = { state.onPrevious() }) {
                     Text("Previous")
                 }
 
@@ -53,16 +95,15 @@ fun Screen(state: PageIndicatorState) {
                     modifier = Modifier
                         .align(Alignment.BottomEnd)
                         .padding(15.dp),
-                    onClick = { state.onNextClick() }) {
+                    onClick = { state.onNext() }) {
                     Text("Next")
                 }
-
             else
                 Button(
                     modifier = Modifier
                         .align(Alignment.BottomEnd)
                         .padding(15.dp),
-                    onClick = { state.onNextClick() }) {
+                    onClick = { state.onNext() }) {
                     Text("Done")
                 }
 
@@ -70,41 +111,61 @@ fun Screen(state: PageIndicatorState) {
     }
 }
 
+@ExperimentalMaterial3Api
+@Composable
+fun rememberPageIndicatorState(
+    titles: List<String> = listOf("First", "Second", "Third", "Fourth"),
+): PageIndicatorState {
+    return rememberSaveable(saver = PageIndicatorState.Saver) {
+        PageIndicatorState(
+            titles
+        )
+    }
+}
+
+
 class PageIndicatorState(
-    val titles: List<String> = listOf("First", "Second", "Third", "Fourth"),
+    val titles: List<String> = listOf("First", "Second", "Third", "Fourth")
+
 ) {
     var current by mutableStateOf(0)
 
-    /* companion object {
-         */
-    /**
-     * The default [Saver] implementation for [TopAppBarState].
-     *//*
-        val Saver: Saver<PageIndicatorState, *> = listSaver(
-            save = { listOf(it.current) },
-            restore = {
+    //save data for configuration changes
+    companion object {
+        val Saver: Saver<PageIndicatorState, *> = mapSaver(
+            save = { pageIndicatorState ->
+                mapOf(
+                    "titles" to pageIndicatorState.titles,
+                    "current" to pageIndicatorState.current,
 
+                    )
+            },
+            restore = {
                 PageIndicatorState(
-                    initialHeightOffsetLimit = it[0],
-                    initialHeightOffset = it[1],
-                    initialContentOffset = it[2]
-                )
+                    titles = it["titles"] as List<String>,
+                ).also { pageIndicatorState -> pageIndicatorState.current = it["current"] as Int }
             }
         )
-    }*/
+    }
 
-    fun onNextClick() {
-        if (current < titles.size) current++
+
+    fun onNext() {
+        if (current < titles.size) current++ //prevent negative
         Log.d("current", current.toString())
     }
 
-    fun onPreviousClick() {
+    fun onPrevious() {
         if (current > 0) current--
     }
 }
 
 @Composable
-fun PageIndicator(pageIndicatorState: PageIndicatorState) {
+fun PageIndicator(
+    pageIndicatorState: PageIndicatorState,
+    indicatorColor: Color = MaterialTheme.colorScheme.primary,
+    onIndicatorColor: Color = MaterialTheme.colorScheme.onPrimary,
+    textColor: Color = Color.Black.copy(alpha = 0.7f)
+) {
     Column {
         Row(
             horizontalArrangement = Arrangement.spacedBy(
@@ -112,7 +173,7 @@ fun PageIndicator(pageIndicatorState: PageIndicatorState) {
             ),
             modifier = Modifier
                 .fillMaxWidth()
-                .background(color = Color.White)
+                .background(color = onIndicatorColor)
                 .padding(vertical = 20.dp, horizontal = 15.dp)
 
         ) {
@@ -126,14 +187,15 @@ fun PageIndicator(pageIndicatorState: PageIndicatorState) {
                         Modifier
                             .size(40.dp)
                             .clip(shape = MaterialTheme.shapes.extraLarge)
-                            .background(MaterialTheme.colorScheme.primary)
+                            .background(indicatorColor)
                     } else {
                         Modifier
                             .size(40.dp)
                             .clip(shape = MaterialTheme.shapes.extraLarge)
-                            .background(Color.White)
+                            .background(onIndicatorColor)
 
                     }
+
                     Surface(
                         shape = MaterialTheme.shapes.extraLarge,
                         shadowElevation = 9.dp, // play with the elevation values
@@ -145,7 +207,7 @@ fun PageIndicator(pageIndicatorState: PageIndicatorState) {
 
                             if (pageIndicatorState.current > index) {
                                 Icon(
-                                    tint = MaterialTheme.colorScheme.onPrimary,
+                                    tint = onIndicatorColor,
                                     imageVector = Icons.Filled.Check,
                                     contentDescription = "done"
                                 )
@@ -154,9 +216,9 @@ fun PageIndicator(pageIndicatorState: PageIndicatorState) {
                                     "${index + 1}",
                                     color =
                                     if (pageIndicatorState.current == index) {
-                                        MaterialTheme.colorScheme.onPrimary
+                                        onIndicatorColor
                                     } else {
-                                        Color.Gray
+                                        textColor
                                     }
                                 )
                             }
@@ -169,7 +231,7 @@ fun PageIndicator(pageIndicatorState: PageIndicatorState) {
                         Text(
                             modifier = Modifier.padding(start = 12.dp),
                             text = title,
-                            color = Color.Black.copy(alpha = 0.7f),
+                            color = textColor,
                             style = MaterialTheme.typography.titleLarge
                         )
                         Spacer(modifier = Modifier.width(35.dp))
@@ -181,34 +243,35 @@ fun PageIndicator(pageIndicatorState: PageIndicatorState) {
         }
 
 
-        val percentage by animateFloatAsState(
-            targetValue = (pageIndicatorState.current.plus(1).toFloat()
-                .div(pageIndicatorState.titles.size))
-        )
-
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(3.dp)
-                .background(Color.Gray)
-        ) {
-            Box(
-                modifier = Modifier
-                    .height(3.dp)
-                    .background(MaterialTheme.colorScheme.primary)
-                    .fillMaxWidth(fraction = percentage)
-            ) {}
-        }
+        PageIndicatorProgressBar(pageIndicatorState, indicatorColor)
     }
 }
 
-
-@Preview
 @Composable
-fun PageIndicatorPreview() {
-    val state = PageIndicatorState(listOf("First", "Second", "Third", "Fourth"))
-    PageIndicator(state)
+private fun PageIndicatorProgressBar(
+    pageIndicatorState: PageIndicatorState,
+    indicatorColor: Color
+) {
+    val percentage by animateFloatAsState(
+        targetValue = (pageIndicatorState.current.plus(1).toFloat()
+            .div(pageIndicatorState.titles.size))
+    )
+
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(3.dp)
+            .background(Color.LightGray)
+    ) {
+        Box(
+            modifier = Modifier
+                .height(3.dp)
+                .background(indicatorColor)
+                .fillMaxWidth(fraction = percentage)
+        ) {}
+    }
 }
+
 
 @Composable
 fun Page(page: Int) {
